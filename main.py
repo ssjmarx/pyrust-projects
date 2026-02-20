03eabe26ea5b9c92c2d55d6e1d3e2334c76c1ec8
Your Name <your.email@example.com>
2026-02-19 20:04:43 -0800

    read random communist manifesto lines!  I'm not sure if this is useful but it's neat.

--- main.py
+++ main.py
@@ -1,40 +1,7 @@
 import rust
 
 def main():
-    say_hello()
-    # print(rust.say_hello(get_secret_word()))
-
-    print(rust.multilingual_greeting(get_preferred_language()))
-
-def get_preferred_language():
-    print("Select your language / Selecciona tu idioma / Sélectionnez votre langue / Wählen Sie Ihre Sprache")
-    print("1. English")
-    print("2. Español")
-    print("3. Français")
-    print("4. Deutsch")
-
-    selection = input("Enter your choice (1-4): ")
-
-    match selection:
-        case "1":
-            return "English"
-        case "2":
-            return "Spanish"
-        case "3":
-            return "French"
-        case "4":
-            return "German"
-        case _:
-            print("Eraro, bonvolu provi denove")
-            return get_preferred_language()
-
-def say_hello():
-    print("Hello!  This is from python.")
-
-def get_secret_word():
-    print("Enter a secret word.")
-    secret_word = input()
-    return secret_word
-
+    pass
+    
 if __name__ == "__main__":
     main()
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello World!  This is a rust package, NOT a rust library.")
+use rand::Rng;
+
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use std::io::BufReader;
+
+fn main() -> io::Result<()> {
+    // Count lines first
+    let f = File::open("../manifesto.txt")?;
+    let reader = BufReader::new(f);
+    let file_length: u32 = reader.lines().count() as u32;
+    
+    // Generate random line number
+    let mut rng = rand::thread_rng();
+    let mut random_line: u32 = rng.gen_range(0..file_length);
+    
+    // Read to the random line
+    let f = File::open("../manifesto.txt")?;
+    let reader = BufReader::new(f);
+
+    // Debug!  Show me the file length and the random line!
+    // println!("File length: {}, Random line: {}", file_length, random_line);
+    
+    // ... read to line random_line
+    let mut counter = 0;
+    for line in reader.lines() {
+        let unwrapped_line = line.unwrap();
+        if counter == random_line {
+            // if the line is empty, get the next one instead
+            if unwrapped_line == "" {
+                random_line += 1;
+                counter += 1;
+            } else {
+            println!("{}", unwrapped_line);
+            break;
+            }
+        } else {
+            counter += 1;
+        }
+        // println!("Counter: {}, Random Line: {}, Line: {}", counter, random_line, unwrapped_line);
+    }
+    
+    Ok(())
 }
